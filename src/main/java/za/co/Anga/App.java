8813861a6a765454cc942ce10463dd74980e2fe9
Anganathi <angangodwane@gmail.com>
2021-04-23 16:48:59 +0200

    first commit

---
+++ src/main/java/za/co/Anga/App.java
@@ -0,0 +1,13 @@
+package za.co.Anga;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
